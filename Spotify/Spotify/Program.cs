b95fad04a8be57075bded2d99a9f4121b1cfd908
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-12-25 19:08:43 +0300

    added spotifyservice, ıspotifyservice, spotfy controller and views

--- Spotify/Spotify/Program.cs
+++ Spotify/Spotify/Program.cs
@@ -1,7 +1,12 @@
+using SpotifyApplicationLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ISpotifyService, SpotifyService>();
+
+
 
 var app = builder.Build();
 
@@ -12,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
