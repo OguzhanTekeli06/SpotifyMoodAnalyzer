fa05ce6074230b486850702de5d53a2741dd4cff
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-12-28 18:34:27 +0300

    mainpage changes

--- Spotify/Spotify/obj/Debug/net8.0/Spotify.AssemblyInfo.cs
+++ Spotify/Spotify/obj/Debug/net8.0/Spotify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4473d8b98a6c37652218dae649a0867c467fb669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc93158922f59d2a880ae1d1a452ede8b19a3b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
