fe387bfadbbe9e3024041ac2084bd640167a0b30
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-12-30 23:33:10 +0300

    UI improved

--- Spotify/Spotify/obj/Debug/net8.0/Spotify.AssemblyInfo.cs
+++ Spotify/Spotify/obj/Debug/net8.0/Spotify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ddc16ca4fd293dfeec4f110b67e92b12df894da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783b4ab5fd549e1a15e7ca4e85d872bec99ed013")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spotify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
